d643740ad557cf5a51214649f6b66a7a593caff0
chulahn <scahn.x@gmail.com>
2018-01-26 15:55:36 -0500

    utilize lifecycle, and calculate timeleft

--- src/Clock.jsx
+++ src/Clock.jsx
@@ -12,10 +12,30 @@ class Clock extends Component {
 			minutes: 0,
 			seconds: 0
 		};
-		console.log(this.props);
+		// console.log(this.props);
+	}
+
+	componentWillMount() {
+		this.getTimeUntil(this.props.deadline)
+	}
+
+	componentDidMount() {
+	}
+
+	getTimeUntil(deadline) {
+		const time = Date.parse(deadline) - Date.parse(new Date());
+		// console.log('time', time);
+		const seconds = Math.floor((time/1000) % 60);
+		const minutes = Math.floor((time/1000/60) % 60);
+		const hours = Math.floor(time/(1000*60*60) % 24);
+		const days = Math.floor(time/(1000*60*60*24));
+
+		// console.log('seconds', seconds, 'minutes', minutes, 'hours', hours, 'days', days)
+		this.setState({days, hours, minutes, seconds})
 	}
 
 	render() {
+
 		return (
 			<div>
 				<div className="Clock-days">{this.state.days} Days</div>
